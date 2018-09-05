622141958d693e1f107590bf642807b802552953
Karl Roth <karoth4@illinois.edu>
2018-09-05 09:05:48 -0500

    updated user interface

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,13 +6,13 @@ router.get('/', function(req, res, next) {
   var fs = require('fs');
   var os = require('os');
   
-  fs.readFile("./user.txt", 'utf8', function(err, data) {
+  fs.readFile("./users.txt", 'utf8', function(err, data) {
       if(err) {
         return console.log(err);
       }
       var user = data.split(os.EOL).filter(val => val != "").slice(-1).pop();
-      res.render('index', {title: 'Hello World Application', name: user })
-  })
+    res.render('index', {title: 'Hello World Application', name: user });
+});
   
 });
 
